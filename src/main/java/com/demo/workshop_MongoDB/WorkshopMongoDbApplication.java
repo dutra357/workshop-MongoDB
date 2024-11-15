e07385056862bbc588e8c61ba2df71be1bad35c0
David Dutra <144396143+dutra357@users.noreply.github.com>
2024-11-15 11:21:32 -0300

    Add files. Spring boot starter project.
    Use in wsl2.

---
+++ src/main/java/com/demo/workshop_MongoDB/WorkshopMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.demo.workshop_MongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopMongoDbApplication.class, args);
+	}
+
+}
